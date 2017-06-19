e9d7acc754a4f7591933f7dfae40cb610662c954
panchangzhi <15563861385@163.com>
2017-06-19 10:35:14 +0800

    直播预告 分享成功隐藏分享提示弹层

--- src/App.vue
+++ src/App.vue
@@ -20,11 +20,7 @@
         title: '\u8499\u725b\u4e13\u5bb6\u5fae\u8bfe\u5802',
         desc: '\u6700\u5177\u4eb2\u548c\u529b\u7684\u533b\u751f\uff0c\u5206\u4eab\u6700\u5b9e\u7528\u7684\u77e5\u8bc6\uff0c\u8ba9\u4f60\u53d8\u6210\u6700\u7701\u5fc3\u7684\u5988\u5988\u3002',
         link: `${window.location.href}?share=1`,
-        imgUrl: 'http://mn.dev.ziseyiliao.com/spa/share_image@2x.png',
-        success () {
-          console.log(this.shareShow)
-          this.shareShow && (this.shareShow = false)
-        }
+        imgUrl: 'http://mn.dev.ziseyiliao.com/spa/share_image@2x.png'
         })
       })
     }
